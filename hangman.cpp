e9f5c4fb875de1a7d08e1779d4448d11050c717c
krzysckh <krzysckh@wp.pl>
2021-04-26 11:07:12 +0200

    added header file. its easier to config now

--- hangman.cpp
+++ hangman.cpp
@@ -1,14 +1,6 @@
-#include <iostream>
-#include <string>
-#include <ctime>
-#ifdef _WIN32
-	#include <windows.h>
-#else
-	#include <unistd.h>
-#endif
+#include "hangman.h"
+
 using namespace std;
-//set amount of words here
-const int ILOSCSLOW = 30;
 
 void clrScr(){
 	#ifdef _WIN32
@@ -24,10 +16,6 @@ int main()
 	cout << "welcome to hangman\n";
 	cout << "you'll have 10 attempts to guess a word!\n";
 	cout << "you can guess a word, but you can also guess single letters\n\n";
-	//WRITE WORDS IN THIS STRING ARRAY!!
-	string slowo[ILOSCSLOW] {"amer","anegdote","germany","jazz","keyboard","word","letter","york","linux","pope","graph", "woman",
-		"guitar","ted","film","gaming","generetor","meme","cat","water","undefined21","undefined22","undefined23","undefined24","undefined25",
-		"undefined26","undefined28","undefined29","undefined30"};
 	string slowoDoOdgadniecia = slowo[rand()%ILOSCSLOW];
 	int dlugoscSlowa = slowoDoOdgadniecia.length();
 	string slowoLiterami[dlugoscSlowa];
@@ -37,10 +25,7 @@ int main()
 	}
 	for (int i = 0; i<dlugoscSlowa; i++){
 		slowoLiterami[i] = slowoDoOdgadniecia.substr(i,1);
-		/*cout << slowoLiterami[i] << endl;*/
 	}
-	//YOU CAN SHANGE NUMBER OF GUESSES IN THIS INITGERRRRRR
-	int iloscProb = 10;
 	bool czyPodanyZnak, czyWygrana, czyPrzegrana, czyKoniec;
 	czyKoniec = 0;
 	czyPrzegrana = 0;
@@ -64,12 +49,11 @@ int main()
 		}
 		if (czyPodanyZnak == 0){
 			if (podaneSlowo == slowoDoOdgadniecia){
-				/*cout << endl << endl << "gratulacje!! twoje slowo to " << slowoDoOdgadniecia << " i udalo ci sie je odgadnąć\n";*/
 				czyWygrana = 1;
 				czyKoniec = 1;
 			} else {
 				iloscProb--;
-				cout << endl << "wrong mateee\n" << endl;
+				cout << endl << "wrong wrong guess\n" << endl;
 			}
 
 		} else {
@@ -82,7 +66,7 @@ int main()
 				}
 			}
 			if (aa == 0){
-				cout << "\n nope\n";
+				cout << "\nnope\n";
 				iloscProb--;
 			} else {
 				iloscProb--;
---
+++ hangman.h
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+
+#ifdef _WIN32
+	#include <windows.h>
+#else
+	#include <unistd.h>
+#endif
+
+//you can set the number of words here
+//tutaj zmień ilość słów
+
+const int ILOSCSLOW = 30;
+
+//you can set the words in this string array. remember to compile the file once again afterwards
+//tu można zmienić używane słowa. pamiętaj o skompilowaniu programu jeszcze raz po zmianie
+
+std::string slowo[ILOSCSLOW] {"amer","anegdote","germany","jazz","keyboard","word","letter","york","linux","pope","graph", "woman",
+	"guitar","ted","film","gaming","generetor","meme","cat","water","undefined21","undefined22","undefined23","undefined24","undefined25",
+	"undefined26","undefined28","undefined29","undefined30"};
+
+
+//you can set number of guesses here
+//tu zmień ilość prób
+
+int iloscProb = 10;
